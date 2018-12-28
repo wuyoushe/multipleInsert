218cd65ca545780762481868c7886bb948129bbf
ZJDM <dengsinkiang@gmail.com>
2018-12-28 15:55:32 +0800

    dxj

---
+++ src/AppBundle/Controller/TestController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use AppBundle\DQL;
+
+/**
+ * Test controller.
+ *
+ * @Route("/api/test")
+ */
+class TestController extends Controller
+{
+    /**
+     * @Route("/new", name="test_new")
+     * @Method("POST")
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function newAction() {
+        $arr = [
+            ['live_id' =>111, 'pv_count' => 1111, 'uv_count' => 1000],
+            ['live_id' =>222, 'pv_count' => 2222, 'uv_count' => 2000],
+            ['live_id' =>333, 'pv_count' => 3333, 'uv_count' => 3000]
+        ];
+        $conDb = $this->get('database_connection');
+        //test 代表表名
+        $dbl = new DQL\MultipleInsert($conDb,'test');
+        //'live_id', 'pv_count', 'uv_count' 字段名
+        $dbl->setColumns(['live_id', 'pv_count', 'uv_count']);
+        $dbl->setValues($arr);
+        $da = $dbl->execute();
+
+        $results = array('code' => 0,'msg' => '更新成功', 'data' => $da);
+        return $this->json($results);
+
+    }
+}
---
+++ src/AppBundle/DQL/MultipleInsert.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace AppBundle\DQL;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Schema\Identifier;
+
+/**
+ * Class MultipleInsert
+ * @package AppBundle\DQL
+ */
+class MultipleInsert
+{
+    /** @var Connection */
+    protected $connection;
+    /** @var Identifier */
+    protected $table;
+    /** @var string[] */
+    protected $columns = [];
+    /** @var array[] */
+    protected $valueSets = [];
+    /** @var int[] PDO::PARAM_* */
+    protected $types = [];
+    /** @var int|null */
+    protected $lastInsertId = null;
+    /** @var int|null */
+    protected $numInsertedRows = null;
+
+    /**
+     * MultipleInsert constructor.
+     * @param Connection $connection
+     * @param $table
+     */
+    public function __construct(Connection $connection, string $table)
+    {
+        $this->connection = $connection;
+        $this->table      = new Identifier($table);
+    }
+
+    /**
+     * @param Connection $connection
+     * @param $tableName
+     *
+     * @return MultipleInsert
+     */
+
+    public static function init(Connection $connection, string $tableName)
+    {
+        return new self($connection, $tableName);
+    }
+
+    /**
+     * set columns
+     *
+     * @param array $columns
+     *
+     * @return $this
+     */
+    public function setColumns(array $columns)
+    {
+        $this->columns = $columns;
+        return $this;
+    }
+
+    /**
+     * set数据
+     *
+     * @param array $valueSets
+     * @param array|null $types
+     *
+     * @return $this
+     */
+    public function setValues(array $valueSets, array $types = null)
+    {
+        $this->valueSets = $valueSets;
+        $this->types     = $types;
+        return $this;
+    }
+
+    /**
+     * 执行
+     *
+     * @return $this
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function execute()
+    {
+        $sql        = $this->getSQL();
+        $parameters = array_reduce($this->valueSets, function (array $flattenedValues, array $valueSet) {
+            return array_merge($flattenedValues, array_values($valueSet));
+        }, []);
+        $this->connection->executeQuery($sql, $parameters, $this->getPositionalTypes());
+        $this->lastInsertId    = $this->connection->lastInsertId();
+        $this->numInsertedRows = count($this->valueSets);
+        return $this;
+    }
+
+    /**
+     * 获取插入的结果id列表
+     *
+     * @return array
+     */
+    public function getLastInsertIds()
+    {
+        $lastInsertIds = [];
+        if (null !== $this->lastInsertId && $this->numInsertedRows > 0) {
+            $lastInsertIds = range(
+                $this->lastInsertId,
+                $this->lastInsertId + $this->numInsertedRows - 1
+            );
+        }
+        return $lastInsertIds;
+    }
+
+    /**
+     * 获取插入总行数
+     *
+     * @return int|null
+     */
+    public function getInsertRows()
+    {
+        return count($this->getLastInsertIds());
+    }
+
+    /**
+     * 创建sql
+     *
+     * @return string
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    protected function getSQL()
+    {
+        $platform       = $this->connection->getDatabasePlatform();
+        $escapedColumns = array_map(function (string $column) use ($platform) {
+            return (new Identifier($column))->getQuotedName($platform);
+        }, $this->columns);
+        // (id, name, ..., date)
+        $columnString = empty($this->columns) ? '' : '(' . implode(', ', $escapedColumns) . ')';
+        // (?, ?, ?, ... , ?)
+        $singlePlaceholder = '(' . implode(', ', array_fill(0, count($this->columns), '?')) . ')';
+        // (?, ?), ... , (?, ?)
+        $placeholders = implode(', ', array_fill(0, count($this->valueSets), $singlePlaceholder));
+        $sql          = sprintf("INSERT INTO %s %s VALUES %s;",
+            $this->table->getQuotedName($platform), $columnString, $placeholders);
+        return $sql;
+    }
+
+    /**
+     * 类型处理
+     *
+     * @return int[] PDO::PARAM_*
+     */
+    protected function getPositionalTypes()
+    {
+        if (empty($this->types)) {
+            return [];
+        }
+        $types           = array_values($this->types);
+        $repeat          = count($this->valueSets);
+        $positionalTypes = [];
+        for ($i = 1; $i <= $repeat; $i++) {
+            $positionalTypes = array_merge($positionalTypes, $types);
+        }
+        return $positionalTypes;
+    }
+}
